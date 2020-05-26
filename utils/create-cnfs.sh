90908a7d64417ffbc059130acf20f900c695a62b
Quim Picó Mora <quim@pop-os.localdomain>
2020-05-26 19:59:17 +0200

    added cnf formulas for ploting

--- utils/create-cnfs.sh
+++ utils/create-cnfs.sh
@@ -1,8 +1,5 @@
-
+#!/bin/bash
 for i in 75 100 125 150 175 200 220 280 300 320 330 340 350; do
-  prove=""
-  SAT="SATISFIABLE"
   ./rnd-cnf-gen.py 75 $i 3 > cnfs/exemple-75-$i.cnf
-  prove=$(./minisat benchmark-folder/exemple-75-$i.cnf 2>&1 /dev/null | grep SATISFIABLE)
 done
 
